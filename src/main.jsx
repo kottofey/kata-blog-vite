ee7c2c036cc7bfc8fe102ae7bbfd37b826bf5bd0
kottofey <romeokontakt@yandex.ru>
2025-01-01 22:30:54 +0300

    [update] Patch Antd to React 19

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { RouterProvider } from 'react-router-dom';
+import '@ant-design/v5-patch-for-react-19';
 
 import './styles/index.scss';
 
